a0adeb0bd99fe08072cc95c69e461887775735fa
Your Name <you@example.com>
2024-05-26 16:31:02 +0000

    added

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    while count < x:
-        try:
-            print('{}'.format(my_list[count]), end='')
-        except IndexError as err:
-            print()
-            return (count)
-
-        count += 1
-    print()
-    return count
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+        return x
+    except IndexError:
+        print()
+        return i
